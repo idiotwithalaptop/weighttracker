2be50014859408367dc68d38da491bc71c30c12b
Ryan Brown <piston47@gmail.com>
2014-10-12 22:24:39 +1100

    Debugging of request body

--- src/routes/api/weighIn.js
+++ src/routes/api/weighIn.js
@@ -19,7 +19,7 @@ router.get('/:userId', function(req, res) {
 router.put('/:userId', function(req, res) {
     var weighIn = req.body;
     data.insertWeighIn(req.params.userId, weighIn);
-    res.json({message: 'Weigh-In Captured', obj: weighIn});
+    res.json({message: 'Weigh-In Captured', req: req});
 });
 
 module.exports = router;
