a2d1a78be3bd2839e9caea7518791e2a0e4d72f8
Ryan Brown <piston47@gmail.com>
2014-10-13 20:03:16 +1100

    Damnit, missed a url

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,11 +6,11 @@ var router = express.Router();
 // Redirect the user to Google for authentication.  When complete, Google
 // will redirect the user back to the application at
 //     /auth/google/return
-router.get('/auth/google', passport.authenticate('google'));
+router.get('/google', passport.authenticate('google'));
 
 // Google will redirect the user to this URL after authentication.  Finish
 // the process by verifying the assertion.  If valid, the user will be
 // logged in.  Otherwise, authentication has failed.
-router.get('/auth/google/return', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+router.get('/google/return', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
 
 module.exports = router;
