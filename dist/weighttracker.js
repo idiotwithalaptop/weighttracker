08e1229bf7ee2a4d208bb2254cfdbe2c1826c1cf
Ryan Brown <piston47@gmail.com>
2014-10-13 20:19:50 +1100

    Revert "Damnit, missed a url"
    This reverts commit a2d1a78be3bd2839e9caea7518791e2a0e4d72f8.

--- dist/weighttracker.js
+++ dist/weighttracker.js
@@ -192,12 +192,12 @@ var router = express.Router();
 // Redirect the user to Google for authentication.  When complete, Google
 // will redirect the user back to the application at
 //     /auth/google/return
-router.get('/google', passport.authenticate('google'));
+router.get('/auth/google', passport.authenticate('google'));
 
 // Google will redirect the user to this URL after authentication.  Finish
 // the process by verifying the assertion.  If valid, the user will be
 // logged in.  Otherwise, authentication has failed.
-router.get('/google/return', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+router.get('/auth/google/return', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
 
 module.exports = router;;var express = require('express');
 var router = express.Router();
